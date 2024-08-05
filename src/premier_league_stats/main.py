54080f7acf5da072da28a711816f223f4fceb03c
eirikeg1 <e.eggset@gmail.com>
2024-08-05 22:52:05 +0200

    Update project structure

--- src/premier_league_stats/main.py
+++ src/premier_league_stats/main.py
@@ -14,6 +14,7 @@ if __name__ == "__main__":
         print(f"Game week {i + 1}:")
         for game in game_week:
             print(f" * {game.id}")
+            print(f"stats: {game.stats}")
         print("\n")
     
     
